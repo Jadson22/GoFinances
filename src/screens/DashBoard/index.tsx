e963b8f63960e94588acd1aab930432d19c686aa
Jadson Medeiros <jadson.asmedeiros@gmail.com>
2021-10-24 00:28:43 -0300

    📦 NEW: typing and accessing card properties

--- src/screens/DashBoard/index.tsx
+++ src/screens/DashBoard/index.tsx
@@ -9,7 +9,8 @@ import {
   User,
   UserGreeting,
   UserName,
-  Icon
+  Icon,
+  HighlightCards
 } from './styles'
 
 const Dashboard = () => {
@@ -29,7 +30,26 @@ const Dashboard = () => {
           <Icon name="power" />
         </UserWrapper>
       </Header>
-      <HighlightCard />
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   )
 }
